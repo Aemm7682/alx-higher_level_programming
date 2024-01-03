fbb866774a4e798e3a45334982309d7e166163d5
Alaa <aemm7682@gmail.com>
2024-01-02 18:41:03 -0800

    c file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h>"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * check_cycle - check if one cycle done
+ * @list: pointer to check list
+ * Return: 1 if one cycle and 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list, *slow = list;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
