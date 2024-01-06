14d44c822e4c9bcd0505b0295c47317f0d3bbd4b
Alaa <aemm7682@gmail.com>
2024-01-05 22:36:13 -0800

    2-args.py 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,13 @@
 import sys
 file_name = sys.argv[0]
 argvs = sys.argv[1:]
-print(len(argvs), end = " ")
-print("arguments:")
+if len(argvs) == 0:
+    print("{} arguments.".format(len(argvs)))
+elif len(argvs) == 1:
+    print(len(argvs), end = " ")
+    print("argument:")
+else:
+    print(len(argvs), end = " ")
+    print("arguments:")
 for i in range(1, (len(argvs) + 1)):
     print("{}: {}".format(i, sys.argv[i]))
