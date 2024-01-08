ebc3fe4eb5863d588052d4e9fc1058b4ec5e2c44
Alaa <aemm7682@gmail.com>
2024-01-08 14:05:00 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    w_out = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] !='C':
+            w_out += my_string[i]
+    return w_out
