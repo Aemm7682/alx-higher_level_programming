2e90e501539e4757167ffbf5e7fe8f1cda133334
Alaa <aemm7682@gmail.com>
2024-02-05 16:46:32 -0800

    4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+'''module for inherits_from'''
+def inherits_from(obj, a_class):
+    '''check if the obj is a true subclass'''
+    return isinstance(obj, a_class) and type(obj) != a_class
