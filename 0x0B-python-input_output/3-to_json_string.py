ef4e066595e52e65ed5612e71743e4c653e42c14
Alaa <aemm7682@gmail.com>
2024-02-14 23:10:15 +0300

    3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Write a function that returns the
+JSON representation of an object (string):'''
+
+
+import json
+def to_json_string(my_obj):
+    '''function to serialize the object by using
+    JSON
+    Argument:
+        my_obj: is the dic'''
+    return json.dumps(my_obj)
