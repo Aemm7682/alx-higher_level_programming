77d6e4ff2d59113d0d514c85d3c21d6035b22cca
Alaa <aemm7682@gmail.com>
2024-02-16 20:06:47 +0300

    10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -8,15 +8,16 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
     def to_json(self, attrs=None):
-        try:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return self.__dict__
-        except Exception:
+        if attrs is None:
             return self.__dict__
-        new_dict = dict()
-        for key, value in self.__dict__.items():
-            new_dict[key] = value
-        return new_dict
+
+        new_dict = {}
+        for attr in attrs:
+            if not isinstance(attr, str):
+                # If any attribute in attrs is not a string, return the entire dictionary
+                return self.__dict__
+            if attr in self.__dict__:
+                new_dict[attr] = self.__dict__[attr]
+            sorted_dict = {key: new_dict[key] for key in sorted(new_dict)}
+        return sorted_dict
