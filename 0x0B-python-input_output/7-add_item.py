596eb0a56758c8f2846a2ccb4734746814961586
Alaa <aemm7682@gmail.com>
2024-02-16 18:08:34 +0300

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 '''a function that creates an Object from a “JSON file'''
+
+
 import sys
-import os
+
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 arglist = list(sys.argv[1:])
@@ -9,5 +12,5 @@ try:
     data_before = load_from_json_file("add_item.json")
 except Exception:
     data_before = []
-data_before_extend(arglist)
-save_to_file(old_data, "add_item.json")
+data_before.extend(arglist)
+save_to_json_file(data_before, "add_item.json")
