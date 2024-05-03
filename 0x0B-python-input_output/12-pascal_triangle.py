8a18236bf63cf098565d2c0ada69bd4cb93f18d6
Alaa <aemm7682@gmail.com>
2024-05-03 18:57:55 +0300

    11-2

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """
-each interior number is the sum of the 
-two numbers above it in the triangle (from the previous row). 
+each interior number is the sum of the
+two numbers above it in the triangle (from the previous row).
 It does this until it has generated n rows.
 """
 
+
 def pascal_triangle(n):
+    """function to creat pascal triangle
+    Args:
+        n (int): number of rows
+        triangle (list): pascal triangle
+        row (list): row of pascal triangle
+        i (int): index of row
+        j (int): index of row
+    """
     if n <= 0:
         return []
     triangle = [[1]]
