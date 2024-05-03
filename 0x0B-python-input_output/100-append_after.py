9a843f3dc06785ecd35ed385f5057c7a16e0b9db
Alaa <aemm7682@gmail.com>
2024-05-03 19:47:22 +0300

    100-2

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Write a function that inserts a line of text to 
-a file, after each line containing a specific 
+Write a function that inserts a line of text to
+a file, after each line containing a specific
 string (see example):"""
 
 
