a1df26bad0b6aeb54fede0aaeb0cba24161c9160
Alaa <aemm7682@gmail.com>
2024-05-05 21:58:54 +0300

    5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a function that prints a text with 2 new lines 
+after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """
+    a function that prints a text with 2 new lines 
+    after each of these characters: ., ? and :
+    Argument:
+    text: is str
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    sando = ['?', ':', '.']
+    for delim in sando:
+        text = text.replace(delim, delim + "\n\n")
+    print("\n".join(line.strip() for line in text.split('\n')))
+
+
+# if __name__ == "__main__":
+#     import doctest
+#     doctest.testfile("test/5-text_indentation.txt")
+# text_indentation(12)
+text_indentation("   ")
