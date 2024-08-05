6b3f4e7bf6721ea51bb8a999f758a006249d1385
Alaa <aemm7682@gmail.com>
2024-08-05 23:59:18 +0300

    102-1

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a function that deletes keys
+with a specific value in a dictionary."""
+
+
+def complex_delete(a_dictionary, value):
+    del_dict =[]
+    for key in a_dictionary:
+        if a_dictionary[key] == value:
+            del_dict.append(key)
+
+    for key_2 in del_dict:
+        del a_dictionary[key_2]
+
+    return a_dictionary
