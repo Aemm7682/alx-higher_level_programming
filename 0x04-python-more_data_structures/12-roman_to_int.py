3922fa2d97d0cfcf0325b79240d894f2a6937c83
Alaa <aemm7682@gmail.com>
2024-08-05 21:59:11 +0300

    12-1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -18,9 +18,9 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if rom_int_dic.get(roman_string[i], 0) == 0:
             return 0
-        if (i != len(roman_string) - 1) and
-        rom_int_dic[roman_string[i]] < rom_int_dic[roman_string[i + 1]]:
-            num += rom_int_dic[roman_string[i]] * -1
+        if (i != len(roman_string) - 1) and \
+                rom_int_dic[roman_string[i]] < rom_int_dic[roman_string[i + 1]]:
+                num += rom_int_dic[roman_string[i]] * -1
         else:
             num += rom_int_dic[roman_string[i]]
 
