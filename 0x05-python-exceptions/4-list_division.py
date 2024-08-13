f55cb02263368437cddddb70319fb1514affa1cd
Alaa <aemm7682@gmail.com>
2024-08-13 19:02:42 +0300

    5-2

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,7 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print("division by 0")
         except TypeError:
-            print("weong type")
+            print("wrong type")
         finally:
             new_list.append(result)
     return new_list
